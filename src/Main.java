104ba5e73177d3e24b5912fda392636d66eb2b0a
DAVIDHUERTAVITELA <al03074239@tecmilenio.mx>
2025-09-15 11:10:12 -0600

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        System.out.printf("\n");
+
+        System.out.printf("Hola, Mundo!");
+
+        System.out.printf("\n");
+        System.out.printf("\n");
+
+        areaRectangulo(1, 2);
+
+        System.out.printf("\n");
+        System.out.printf("\n");
+
+        System.out.printf("Creado por: DAVID HUERTA VITELA");
+
+        System.out.printf("\n");
+    }
+
+    public static void areaRectangulo(int a, int b) {
+
+        System.out.printf("Siendo a = " + a + " metro(s), y b = " + b + " metro(s).");
+
+        System.out.printf("\n");
+
+        System.out.printf("El area es: " + a * b + " metros cuadrados");
+
     }
 }
